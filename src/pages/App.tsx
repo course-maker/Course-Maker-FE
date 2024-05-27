1d5a076e3da4ece74eec5e8d50e3a5ab2dc1c2a4
김하은 <han.kim0904@gmail.com>
2024-05-27 15:33:25 +0900

    Chore: SCSS 세팅

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import AppLayout from "../layout/AppLayout";
 import HomePage from "./HomePage";
+import ScssExample from "./scss-example";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<AppLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="scss-example" element={<ScssExample />} />
       </Route>
     </Routes>
   );
