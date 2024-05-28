276257fea6face2e0a3a9b14f004b94b75f72562
김하은 <han.kim0904@gmail.com>
2024-05-29 08:58:11 +0900

    Chore: pagePath, navegationBarMenu constants 파일 만들기

--- src/components/commons/NavigationBar/index.tsx
+++ src/components/commons/NavigationBar/index.tsx
@@ -1,5 +1,13 @@
+import { IMAGES } from "@/constants/images";
+import Image from "../Image";
+
 const NavigationBar = () => {
-  return <div>index</div>;
+  return (
+    <>
+      <Image imageInfo={IMAGES.courseMakerLogo} />
+      //여기 컴포넌트 만들어야 할 수도 (클릭 버튼?) Link 태그 쓰기!!
+    </>
+  );
 };
 
 export default NavigationBar;
