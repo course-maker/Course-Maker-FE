a56d1f9b7584d2409c0ed73053e00000a1ed7c4c
김하은 <han.kim0904@gmail.com>
2024-05-28 18:23:42 +0900

    Chore: Button.spec.tsx의 Button 컴포넌트 import 경로 변경

--- src/__tests__/unit/Button.spec.tsx
+++ src/__tests__/unit/Button.spec.tsx
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import Button, { ButtonProps } from "@/components/commons/button";
+import Button, { ButtonProps } from "@/components/commons/Button";
 import { vi } from "vitest";
 
 describe("Button Component", () => {
