ff3b39d6e1782bc55e11852c44803c2b73e15e1d
박지원 <jackgg12322@gmail.com>
2024-05-31 22:34:10 +0900

    Chore: 상세페이지에 사용 할 이미지 추가

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,5 +1,9 @@
 import courseMakerLogo from "@/assets/images/course_maker_logo.svg";
 import kakaoOauthButton from "@/assets/images/kakao_oauth_button.svg";
+import heartIcon from "@/assets/images/heart.svg";
+import thumbsUpIcon from "@/assets/images/thumbs_up.svg";
+import starIcon from "@/assets/images/star.svg";
+import linkCopyIcon from "@/assets/images/link_copy.svg";
 
 export const IMAGES = {
   courseMakerLogo: {
@@ -9,5 +13,21 @@ export const IMAGES = {
   kakaoOauthButton: {
     src: kakaoOauthButton,
     alt: "카카오톡 OAuth 버튼",
+  },
+  heartIcon: {
+    src: heartIcon,
+    alt: "하트 아이콘",
+  },
+  thumbsUpIcon: {
+    src: thumbsUpIcon,
+    alt: "좋아요 아이콘",
+  },
+  starIcon: {
+    src: starIcon,
+    alt: "별점 아이콘",
+  },
+  linkCopyIcon: {
+    src: linkCopyIcon,
+    alt: "링크 복사 아이콘",
   },
 };
