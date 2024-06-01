4b02cc0a56be5720ccba3f7a95ca90849c86dc61
박지원 <jackgg12322@gmail.com>
2024-06-01 15:05:50 +0900

    Feat: TitleBox 컴포넌트 구현

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,8 +1,11 @@
 import courseMakerLogo from "@/assets/images/course_maker_logo.svg";
 import kakaoOauthButton from "@/assets/images/kakao_oauth_button.svg";
-import heartIcon from "@/assets/images/heart.svg";
-import thumbsUpIcon from "@/assets/images/thumbs_up.svg";
-import starIcon from "@/assets/images/star.svg";
+import whiteHeartIcon from "@/assets/images/white_heart.svg";
+import whiteThumbsUpIcon from "@/assets/images/white_thumbs_up.svg";
+import whiteStarIcon from "@/assets/images/white_star.svg";
+import blackHeartIcon from "@/assets/images/black_heart.svg";
+import blackThumbsUpIcon from "@/assets/images/black_thumbs_up.svg";
+import blackStarIcon from "@/assets/images/black_star.svg";
 import linkCopyIcon from "@/assets/images/link_copy.svg";
 
 export const IMAGES = {
@@ -15,15 +18,27 @@ export const IMAGES = {
     alt: "카카오톡 OAuth 버튼",
   },
   heartIcon: {
-    src: heartIcon,
+    src: whiteHeartIcon,
     alt: "하트 아이콘",
   },
   thumbsUpIcon: {
-    src: thumbsUpIcon,
+    src: whiteThumbsUpIcon,
     alt: "좋아요 아이콘",
   },
   starIcon: {
-    src: starIcon,
+    src: whiteStarIcon,
+    alt: "별점 아이콘",
+  },
+  blackHeartIcon: {
+    src: blackHeartIcon,
+    alt: "하트 아이콘",
+  },
+  blackThumbsUpIcon: {
+    src: blackThumbsUpIcon,
+    alt: "좋아요 아이콘",
+  },
+  blackStarIcon: {
+    src: blackStarIcon,
     alt: "별점 아이콘",
   },
   linkCopyIcon: {
