f9058941d6fad322e067cc771b698215828dbe0f
박지원 <jackgg12322@gmail.com>
2024-06-02 20:19:40 +0900

    Feat: 여행지 상세페이지 UI 구현

---
+++ src/pages/SpotDetailPage/index.tsx
@@ -0,0 +1,7 @@
+import SpotDetailLayout from "@/layout/DetailPageLayout/Spot/SpotDetailLayout";
+
+const SpotDetailPage = () => {
+  return <SpotDetailLayout />;
+};
+
+export default SpotDetailPage;
