d8567f6d4dab8a05054e99e007b13f8c6789619a
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-06-04 18:57:22 +0900

    Feat: "필터, 여행지 리스트 UI"

--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -3,11 +3,25 @@ import styles from "./SearchPage.module.scss";
 import classNames from "classnames/bind";
 import BadgeList from "@/components/commons/BadgeList/BadgeList";
 import Section from "@/components/commons/Section/Section";
+import Card from "@/components/commons/Card/Card";
 import data from "./data.json";
+import listData from "./listData.json";
+
+interface Icons {
+  [key: string]: number;
+}
+
+interface MockData {
+  id: number;
+  title: string;
+  location: string;
+  icons: Icons;
+}
 
 const cx = classNames.bind(styles);
 const courseData = data.courseData;
 const destinationData = data.destinationData;
+const lists: MockData[] = listData;
 
 const SearchPage = () => {
   const [activeTab, setActiveTab] = useState("코스 찾기");
@@ -36,32 +50,50 @@ const SearchPage = () => {
         </div>
       </div>
 
-      <Section title="" className={cx("tab-container")}>
-        {activeTab === "코스 찾기" ? (
-          <>
-            {Object.entries(courseData).map(([title, badges]) => (
-              <BadgeList
-                key={title}
-                title={title}
-                badges={badges}
-                selectedBadges={selectedBadges}
-                toggleBadge={toggleBadge}
-              />
-            ))}
-          </>
-        ) : (
-          <>
-            {Object.entries(destinationData).map(([title, badges]) => (
-              <BadgeList
-                key={title}
-                title={title}
-                badges={badges}
-                selectedBadges={selectedBadges}
-                toggleBadge={toggleBadge}
-              />
-            ))}
-          </>
-        )}
+      {activeTab === "코스 찾기" ? (
+        <Section title="" className={cx("tab-container")}>
+          {Object.entries(courseData).map(([title, badges]) => (
+            <BadgeList
+              key={title}
+              title={title}
+              badges={badges}
+              selectedBadges={selectedBadges}
+              toggleBadge={toggleBadge}
+            />
+          ))}
+        </Section>
+      ) : (
+        <Section title="" className={cx("tab-container")}>
+          {Object.entries(destinationData).map(([title, badges]) => (
+            <BadgeList
+              key={title}
+              title={title}
+              badges={badges}
+              selectedBadges={selectedBadges}
+              toggleBadge={toggleBadge}
+            />
+          ))}
+        </Section>
+      )}
+
+      <div className={cx("option-container")}>
+        <span>전체 {"1개"}</span>
+        <div>
+          <select name="HeadlineAct" id="HeadlineAct" className={cx("select-box")}>
+            <option value="">최신순</option>
+            <option value="JM">인기순</option>
+            <option value="SRV">조회수순</option>
+            <option value="JH">별점순</option>
+          </select>
+        </div>
+      </div>
+
+      <Section title="">
+        <div className={cx("card_container")}>
+          {lists.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
       </Section>
     </div>
   );
