9ecc47179a1054f892ecc67f334a9ae913b8df1c
박지원 <jackgg12322@gmail.com>
2024-06-05 17:14:41 +0900

    Feat: Stepper 컴포넌트 구현

---
+++ src/components/commons/Steppers/Stepper/Stepper.tsx
@@ -0,0 +1,43 @@
+import { useStepper } from "../StepperContext";
+import { FaCheck } from "react-icons/fa6";
+import styles from "./Stepper.module.scss";
+import classNames from "classnames/bind";
+import NavigationButtons from "../NavigationButtons/NavigationButtons";
+
+const cx = classNames.bind(styles);
+
+interface StepperProps {
+  children: React.ReactNode;
+}
+
+const Stepper = ({ children }: StepperProps) => {
+  const { currentStep } = useStepper();
+
+  return (
+    <>
+      <div className={cx("stepIndicator")}>
+        <div>
+          <FaCheck className={cx("check-icon")} />
+          <div className={cx("stepCircle")}></div>
+          <div className={cx("stepLabel")}>Step1</div>
+        </div>
+        <div>
+          <FaCheck className={cx("check-icon")} />
+          <div className={cx(currentStep >= 2 ? "stepCircle" : "inactiveCircle")}></div>
+          <div className={cx(currentStep >= 2 ? "step-line" : "inactive-line")}></div>
+          <div className={cx("stepLabel")}>Step2</div>
+        </div>
+        <div>
+          <FaCheck className={cx("check-icon")} />
+          <div className={cx(currentStep === 3 ? "stepCircle" : "inactiveCircle")}></div>
+          <div className={cx(currentStep === 3 ? "step-line" : "inactive-line")}></div>
+          <div className={cx("stepLabel")}>Step3</div>
+        </div>
+      </div>
+      {children}
+      <NavigationButtons />
+    </>
+  );
+};
+
+export default Stepper;
