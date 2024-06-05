a7c60ae2c26d64c4124a58678b63324399351432
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-06-06 00:04:46 +0900

    Feat: "카드 로딩 시 스켈레톤 처리, param 값 수정"
    - 조회수, 일정, 멤버 등의 정보는 고도화때 도입 예정이라 뱃지 정보들로 대체함

--- src/components/commons/ItemBox/ItemBox.tsx
+++ src/components/commons/ItemBox/ItemBox.tsx
@@ -1,34 +1,41 @@
 import classNames from "classnames/bind";
 import styles from "./ItemBox.module.scss";
-import Image from "@/components/commons/Image";
-import { IMAGES } from "@/constants/images";
+import Badge from "@/components/commons/Badge/Badge";
+// import Image from "@/components/commons/Image";
+// import { IMAGES } from "@/constants/images";
 
 const cx = classNames.bind(styles);
 
-interface Icons {
-  [key: string]: number;
+// interface Icons {
+//   [key: string]: number;
+// }
+
+interface Tag {
+  id: number;
+  name: string;
+  description: string;
 }
 
 interface ItemBoxProps {
   location: string;
   title?: string;
-  icons: Icons;
+  tags: Tag[];
 }
 
-const iconMapping: { [key: string]: any } = {
-  blackHeart: IMAGES.blackHeartIcon,
-  blackThumbsUp: IMAGES.blackThumbsUpIcon,
-  blackStar: IMAGES.blackStarIcon,
-  heart: IMAGES.heartIcon,
-  thumbsUp: IMAGES.thumbsUpIcon,
-  star: IMAGES.starIcon,
-  location: IMAGES.locationIcon,
-  linkCopy: IMAGES.linkCopyIcon,
-  calendar: IMAGES.calendarIcon,
-  member: IMAGES.memberIcon,
-};
+// const iconMapping: { [key: string]: any } = {
+//   blackHeart: IMAGES.blackHeartIcon,
+//   blackThumbsUp: IMAGES.blackThumbsUpIcon,
+//   blackStar: IMAGES.blackStarIcon,
+//   heart: IMAGES.heartIcon,
+//   thumbsUp: IMAGES.thumbsUpIcon,
+//   star: IMAGES.starIcon,
+//   location: IMAGES.locationIcon,
+//   linkCopy: IMAGES.linkCopyIcon,
+//   calendar: IMAGES.calendarIcon,
+//   member: IMAGES.memberIcon,
+// };
 
-const ItemBox = ({ location, title, icons }: ItemBoxProps) => {
+const ItemBox = ({ location, title, tags }: ItemBoxProps) => {
   return (
     <div className={cx("item-box")}>
       <div className={cx("title-group")}>
@@ -36,12 +43,18 @@ const ItemBox = ({ location, title, icons }: ItemBoxProps) => {
         <p className={cx("item-location")}>{location}</p>
       </div>
       <div className={cx("score-group")}>
-        {Object.entries(icons).map(([key, value]) => (
+        {tags.map((tag) => (
+          <Badge key={tag.id} color="gray" variant="primary" size="xsmall">
+            {tag.name}
+          </Badge>
+        ))}
+        {/* 아이콘 고도화때 작업 예정 
+        {Object.entries(tags).map(([key, value]) => (
           <span key={key} className={cx("score-item")}>
             <Image imageInfo={iconMapping[key]} />
             {value}
           </span>
-        ))}
+        ))} */}
       </div>
     </div>
   );
