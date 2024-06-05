a39a8c39b67a0076ee00a83f4c9efa7203f6fd61
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-06-05 15:47:47 +0900

    Feat:"홈페이지 이미지 추가"

---
+++ src/pages/HomePage/data.ts
@@ -0,0 +1,114 @@
+import { IMAGES } from "@/constants/images";
+
+export const bannerData = [
+  {
+    id: 0,
+    image: IMAGES.Banner1.src,
+    size: "x-large",
+  },
+  {
+    id: 1,
+    image: IMAGES.Banner1.src,
+    size: "x-large",
+  },
+];
+
+export const busanData = [
+  {
+    id: 0,
+    title: "부산 아쿠아리움",
+    location: "부산 해운대구 해운대해변로 266",
+    icons: {
+      calendar: 2,
+      member: 2,
+      blackStar: 2,
+    },
+  },
+  {
+    id: 1,
+    title: "해운대",
+    location: "부산 해운대구 해운대해변로 280",
+    icons: {
+      calendar: 102,
+      member: 21,
+      blackStar: 222,
+    },
+  },
+  {
+    id: 2,
+    title: "부산 영도",
+    location: "부산 해운대구 해운대해변로 299",
+    icons: {
+      calendar: 22,
+      member: 27,
+      blackStar: 12,
+    },
+  },
+  {
+    id: 3,
+    title: "부산 광안리",
+    location: "부산 해운대구 해운대해변로 388",
+    icons: {
+      calendar: 72,
+      member: 827,
+      blackStar: 1332,
+    },
+  },
+];
+
+export const bannerItemsData = {
+  small: [
+    {
+      id: 0,
+      image: IMAGES.Rectangle_40.src,
+      title: "유아동반",
+      size: "small",
+    },
+    {
+      id: 1,
+      image: IMAGES.Rectangle_41.src,
+      title: "문화/유적지",
+      size: "small",
+    },
+    {
+      id: 2,
+      image: IMAGES.Rectangle_42.src,
+      title: "무장해여행",
+      size: "small",
+    },
+    {
+      id: 3,
+      image: IMAGES.Rectangle_43.src,
+      title: "애견동반",
+      size: "small",
+    },
+    {
+      id: 4,
+      image: IMAGES.Rectangle_44.src,
+      title: "축제",
+      size: "small",
+    },
+    {
+      id: 5,
+      image: IMAGES.Rectangle_45.src,
+      title: "식도락",
+      size: "small",
+    },
+  ],
+  large: [
+    {
+      id: 0,
+      image: IMAGES.Home_right_test.src,
+      subtitle: "스카이캡슐",
+      title: "바다를 따라 다니며\n육지와는 다른 풍경을 즐기세요",
+      size: "large",
+    },
+    {
+      id: 1,
+      image: IMAGES.Home_left_test.src,
+      subtitle: "부산 필수코스",
+      title: "부산이 처음이라면\n여기부터 가보셔야죠",
+      size: "large",
+    },
+  ],
+};
