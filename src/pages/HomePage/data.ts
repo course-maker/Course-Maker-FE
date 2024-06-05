a974402f3db76100402611f48f60f3d706713c49
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-06-06 00:10:20 +0900

    Feat: "홈페이지 swiper 적용"

--- src/pages/HomePage/data.ts
+++ src/pages/HomePage/data.ts
@@ -16,43 +16,59 @@ export const bannerData = [
 export const busanData = [
   {
     id: 0,
-    title: "부산 아쿠아리움",
+    name: "부산 아쿠아리움",
     location: "부산 해운대구 해운대해변로 266",
     icons: {
       calendar: 2,
       member: 2,
       blackStar: 2,
     },
+    tags: [
+      { id: 0, name: "자연", description: "바다" },
+      { id: 1, name: "자연", description: "둘레길" },
+    ],
   },
   {
     id: 1,
-    title: "해운대",
+    name: "해운대",
     location: "부산 해운대구 해운대해변로 280",
     icons: {
       calendar: 102,
       member: 21,
       blackStar: 222,
     },
+    tags: [
+      { id: 0, name: "자연", description: "바다" },
+      { id: 1, name: "동행", description: "유아동반" },
+    ],
   },
   {
     id: 2,
-    title: "부산 영도",
+    name: "부산 영도",
     location: "부산 해운대구 해운대해변로 299",
     icons: {
       calendar: 22,
       member: 27,
       blackStar: 12,
     },
+    tags: [
+      { id: 0, name: "자연", description: "바다" },
+      { id: 1, name: "날씨", description: "실내공간" },
+    ],
   },
   {
     id: 3,
-    title: "부산 광안리",
+    name: "부산 광안리",
     location: "부산 해운대구 해운대해변로 388",
     icons: {
       calendar: 72,
       member: 827,
       blackStar: 1332,
     },
+    tags: [
+      { id: 0, name: "자연", description: "바다" },
+      { id: 1, name: "음식", description: "전통시장" },
+    ],
   },
 ];
 
