b7e03ed9cc45d77f73af88e80c6728b63c02747d
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-06-13 02:06:12 +0900

    Fix: "타입에러 수정"

--- src/constants/initialValues.ts
+++ src/constants/initialValues.ts
@@ -3,26 +3,16 @@ import { Courses } from "@/api/course/type";
 
 //여행지 초기값 설정
 export const initialDestination: Destination = {
-  id: 0,
-  name: "",
-  location: "",
-  pictureLink: "",
-  content: "",
+  currentPage: 1,
+  totalPage: 1,
+  pagingSlice: 20,
+  contents: [],
 };
 
 //코스 초기값 설정
 export const initialCourse: Courses = {
-  id: 0,
-  title: "",
-  content: "",
-  views: 0,
-  duration: 0,
-  travelerCount: 0,
-  travelType: 0,
-  pictureLink: "",
-  courseDestinations: [],
-  courseTags: [],
-  member: {
-    nickname: "",
-  },
+  currentPage: 1,
+  totalPage: 1,
+  pagingSlice: 20,
+  contents: [],
 };
