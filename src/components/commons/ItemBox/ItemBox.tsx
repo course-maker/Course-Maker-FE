22d5c1ea0c3d76fd7ccd513e76b1633d4aaee416
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-06-14 19:20:34 +0900

    Feat: "코스 box들 하단에 여행기간과 여행 추천인원"

--- src/components/commons/ItemBox/ItemBox.tsx
+++ src/components/commons/ItemBox/ItemBox.tsx
@@ -3,7 +3,8 @@ import styles from "./ItemBox.module.scss";
 import Badge from "@/components/commons/Badge/Badge";
 
 import { tagResponseDto } from "@/api/tag/type";
-
+import { IMAGES } from "@/constants/images";
+import Image from "@/components/commons/Image";
 const cx = classNames.bind(styles);
 
 interface Location {
@@ -15,22 +16,45 @@ interface Location {
 interface ItemBoxProps {
   location: Location;
   title?: string;
-  tags: tagResponseDto[];
+  tags?: tagResponseDto[];
+  travelerCount?: number;
+  views?: number;
+  duration?: number;
+  name?: string;
 }
 
-const ItemBox: React.FC<ItemBoxProps> = ({ location, title, tags }) => {
+const ItemBox: React.FC<ItemBoxProps> = ({ location, title, tags, name, travelerCount, views, duration }) => {
   return (
     <div className={cx("item-box")}>
       <div className={cx("title-group")}>
         <p className={cx("item-title")}>{title}</p>
-        <p className={cx("item-location")}>{location.address}</p>
+        {name === "코스 찾기" ? (
+          <p className={cx("item-location")}>조회수 {views} 회</p>
+        ) : (
+          <p className={cx("item-location")}>{location.address}</p>
+        )}
       </div>
-      <div className={cx("score-group")}>
-        {tags?.slice(0, 3).map((tag) => (
-          <Badge key={tag.id} color="gray" variant="primary" size="xsmall">
-            {tag.name}
-          </Badge>
-        ))}
+      <div>
+        {name === "코스 찾기" ? (
+          <div className={cx("score-group")}>
+            <div className={cx("score-item-course")}>
+              <Image imageInfo={IMAGES.calendarIcon} />
+              <span className={cx("score-text")}>{duration}일</span>
+            </div>
+            <div className={cx("score-item-course")}>
+              <Image imageInfo={IMAGES.memberIcon} />
+              <span className={cx("score-text")}>{travelerCount}인</span>
+            </div>
+          </div>
+        ) : (
+          <div className={cx("score-group")}>
+            {tags?.slice(0, 3).map((tag) => (
+              <Badge key={tag.id} color="gray" variant="primary" size="xsmall">
+                {tag.name}
+              </Badge>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
