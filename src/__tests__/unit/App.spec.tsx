3b47cb7e62d0adf7c95e489a3d1574d1f9ee6cba
김하은 <han.kim0904@gmail.com>
2024-06-25 12:24:08 +0900

    Test: 코드 스플리팅을 고려하도록 테스트 코드 수정

--- src/__tests__/unit/App.spec.tsx
+++ src/__tests__/unit/App.spec.tsx
@@ -1,15 +1,14 @@
+import App from "@/pages/App";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-import App from "@/pages/App";
 
-test("renders the Home Page", () => {
+test("renders the Home Page", async () => {
   render(
     <MemoryRouter initialEntries={["/"]}>
       <App />
     </MemoryRouter>,
   );
 
-  // Home Page 컴포넌트가 렌더링되었는지 확인
-  const headingElement = screen.getByTestId("home-page");
+  const headingElement = await screen.findByTestId("home-page");
   expect(headingElement).toBeInTheDocument();
 });
