81e5c99d822ecedb059129abafded242550bf94a
김하은 <han.kim0904@gmail.com>
2024-07-02 23:59:33 +0900

    Refactor: sign 페이지에서 navBar 제거

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,6 +1,7 @@
 import { PAGE_PATH } from "@/constants/pagePath";
 import { authState } from "@/recoil/authAtom";
 import { getAccessToken } from "@/utils/manageTokenInfo";
+import { isAuthPage, isRegisterPage } from "@/utils/pageHelpers";
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
@@ -9,15 +10,6 @@ const useAuth = () => {
   const [isAuth, setIsAuth] = useRecoilState(authState);
   const location = useLocation();
   const navigate = useNavigate();
-  const { signIn, signUp, courseRegister, destinationRegister } = PAGE_PATH;
-
-  const authPages = [signIn, signUp];
-  const registerPages = [courseRegister, destinationRegister];
-  const dynamicPages = [/\/course\/[^/]+\/edit/, /\/destination\/[^/]+\/edit/];
-
-  const isAuthPage = (path: string) => authPages.includes(path);
-  const isRegisterPage = (path: string) =>
-    registerPages.includes(path) || dynamicPages.some((regex) => regex.test(path));
 
   useEffect(() => {
     const accessToken = getAccessToken();
@@ -26,7 +18,7 @@ const useAuth = () => {
     if (accessToken && isAuthPage(location.pathname)) {
       navigate("/");
     } else if (!accessToken && isRegisterPage(location.pathname)) {
-      navigate(signIn);
+      navigate(PAGE_PATH.signIn);
     }
   }, [location.pathname, setIsAuth, navigate]);
 
--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,19 +1,24 @@
-import { Outlet } from "react-router-dom";
-import styles from "./AppLayout.module.scss";
-import classNames from "classnames/bind";
 import NavigationBar from "@/components/commons/NavigationBar";
 import useAuth from "@/hooks/useAuth";
+import { isAuthPage } from "@/utils/pageHelpers";
+import classNames from "classnames/bind";
+import { Outlet, useLocation } from "react-router-dom";
+import styles from "./AppLayout.module.scss";
 
 const cx = classNames.bind(styles);
 
 const AppLayout = () => {
   const { isAuth } = useAuth();
+  const { pathname } = useLocation();
+  const isSignPage = isAuthPage(pathname);
 
   return (
     <>
-      <nav className={cx("nav")}>
-        <NavigationBar isAuth={isAuth} />
-      </nav>
+      {isSignPage || (
+        <nav className={cx("nav")}>
+          <NavigationBar isAuth={isAuth} />
+        </nav>
+      )}
       <main className={cx("main")}>
         <div className={cx("empty")}></div>
         <Outlet />
