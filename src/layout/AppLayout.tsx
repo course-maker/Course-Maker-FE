453335aba007743a55abffc22145bb2b247db58a
김하은 <han.kim0904@gmail.com>
2024-07-04 21:17:24 +0900

    Refactor: 로그인, 회원가입 페이지에는 navBar가 보이지 않도록 수정함

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,6 +1,6 @@
 import NavigationBar from "@/components/commons/NavigationBar";
 import useAuth from "@/hooks/useAuth";
-import { isAuthPage } from "@/utils/pageHelpers";
+import { isSignPage } from "@/utils/pageHelpers";
 import classNames from "classnames/bind";
 import { Outlet, useLocation } from "react-router-dom";
 import styles from "./AppLayout.module.scss";
@@ -10,19 +10,22 @@ const cx = classNames.bind(styles);
 const AppLayout = () => {
   const { isAuth } = useAuth();
   const { pathname } = useLocation();
-  const isSignPage = isAuthPage(pathname);
 
   return (
     <>
-      {isSignPage || (
-        <nav className={cx("nav")}>
-          <NavigationBar isAuth={isAuth} />
-        </nav>
-      )}
-      <main className={cx("main")}>
-        <div className={cx("empty")}></div>
+      {isSignPage(pathname) ? (
         <Outlet />
-      </main>
+      ) : (
+        <>
+          <nav className={cx("nav")}>
+            <NavigationBar isAuth={isAuth} />
+          </nav>
+          <main className={cx("main")}>
+            <div className={cx("empty")}></div>
+            <Outlet />
+          </main>
+        </>
+      )}
     </>
   );
 };
