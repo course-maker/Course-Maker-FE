3f312503ebbb179f992072e44f4a7f0611a43902
박지원 <jackgg12322@gmail.com>
2024-07-22 18:56:19 +0900

    Feat: "코스 상세페이지 전체 여행지 영역 구현"

---
+++ src/components/domains/DetailPage/CourseDetail/CourseDetailInfo/CourseDetailInfo.tsx
@@ -0,0 +1,83 @@
+import { getCourseDetail } from "@/api/course";
+import { AllCardList } from "@/components/commons/CardList/CardList";
+import { useQuery } from "@tanstack/react-query";
+import { Link, useParams } from "react-router-dom";
+import styles from "./CourseDetailInfo.module.scss";
+import classNames from "classnames/bind";
+import Image from "../../../../commons/Image/index";
+import { IMAGES } from "@/constants/images";
+
+const cx = classNames.bind(styles);
+
+const CourseDetailInfo = () => {
+  const { id } = useParams<{ id: string }>();
+
+  const {
+    data: courseDetailData,
+    isError,
+    isSuccess,
+  } = useQuery({
+    queryKey: ["courseDetailData"],
+    queryFn: () => getCourseDetail(Number(id)),
+    retry: 0,
+  });
+
+  if (isSuccess) {
+    courseDetailData;
+  } else {
+    if (isError) {
+      isError;
+    }
+  }
+  const ratingData = [
+    { icon: IMAGES.BlackFavoriteIcon, value: "320" },
+    { icon: IMAGES.BlackBookmarkIcon, value: "121" },
+    { icon: IMAGES.BlackStarIcon, value: "4.2" },
+  ];
+
+  return (
+    <div className={cx("info-container")}>
+      <div className={cx("course-img")}>
+        <img className={cx("course")} src={courseDetailData?.pictureLink} alt="코스상세페이지 대표 이미지" />
+      </div>
+      <div className={cx("destination-group")}>
+        <div className={cx("title-group")}>
+          <p className={cx("title")}>전체 여행지</p>
+          <p className={cx("sub-title")}>여행지를 클릭하면 여행지 상세페이지를 확인할 수 있습니다.</p>
+        </div>
+        <AllCardList>
+          {courseDetailData &&
+            courseDetailData.courseDestinations.map((item, id) => (
+              <Link to={`/destination/${item.destination.id}`} key={id}>
+                <div className={cx("travel-group")}>
+                  <div className={cx("travel-img")}>
+                    <img
+                      className={cx("travel")}
+                      src={item?.destination?.pictureLink}
+                      alt={`${item?.destination?.name} 이미지`}
+                    />
+                  </div>
+                  <div className={cx("travel-info")}>
+                    <div className={cx("info-group")}>
+                      <p className={cx("name")}>{item?.destination?.name}</p>
+                      <p className={cx("address")}>{item?.destination?.location?.address}</p>
+                    </div>
+                    <div className={cx("rating-group")}>
+                      {ratingData.map((item, index) => (
+                        <span key={index} className={cx("rating-icon")}>
+                          <Image imageInfo={item.icon} />
+                          <span>{item.value}</span>
+                        </span>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            ))}
+        </AllCardList>
+      </div>
+    </div>
+  );
+};
+
+export default CourseDetailInfo;
