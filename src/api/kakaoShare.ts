df88de0332f356f899748bdba77dc8a6096e5b79
박지원 <jackgg12322@gmail.com>
2024-07-22 15:56:05 +0900

    Feat: "카카오 공유하기 기능 구현"

---
+++ src/api/kakaoShare.ts
@@ -0,0 +1,43 @@
+interface ShareMessageProps {
+  id: number;
+  title: string;
+  description: string;
+  imageUrl: string;
+  pageType: "course" | "destination";
+}
+
+function shareMessage({ id, title, description, imageUrl, pageType }: ShareMessageProps): void {
+  if (window.Kakao) {
+    const kakao = window.Kakao;
+
+    const baseUrl = "http://localhost:5173"; // 실제 배포 시 이 URL을 변경해야 합니다.
+    const url = `${baseUrl}/${pageType}/${id}`;
+
+    kakao.Share.sendDefault({
+      objectType: "feed",
+      content: {
+        title: title || (pageType === "course" ? "코스메이커 코스" : "코스메이커 여행지"),
+        description:
+          description || (pageType === "course" ? "멋진 코스를 확인해보세요!" : "멋진 여행지를 확인해보세요!"),
+        imageUrl: imageUrl || "https://ifh.cc/v-sFhRjF",
+        link: {
+          mobileWebUrl: url,
+          webUrl: url,
+        },
+      },
+      buttons: [
+        {
+          title: pageType === "course" ? "코스 상세보기" : "여행지 상세보기",
+          link: {
+            mobileWebUrl: url,
+            webUrl: url,
+          },
+        },
+      ],
+    });
+  } else {
+    console.error("Kakao SDK가 로드되지 않았습니다.");
+  }
+}
+
+export default shareMessage;
