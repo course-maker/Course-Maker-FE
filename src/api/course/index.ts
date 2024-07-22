a3b21c5db5851da8c02effa46e819ff4e863c2c3
박지원 <jackgg12322@gmail.com>
2024-07-22 15:56:53 +0900

    Feat: "코스 삭제하기 api 추가"

--- src/api/course/index.ts
+++ src/api/course/index.ts
@@ -6,9 +6,13 @@ import { Courses, Course } from "./type";
 //코스 정보
 export const getCourse = (params: string): Promise<Courses> => apiRequest("get", `${coursesAddress.get}?${params}`);
 
-/**여행지 상세정보 조회하기*/
+/**코스 상세정보 조회하기*/
 export const getCourseDetail = (postId: number): Promise<Course> =>
   apiRequest("get", `${coursesAddress.get}/${postId}`);
+
+/**코스 삭제하기*/
+export const deleteCourseDetail = (postId: number): Promise<Course> =>
+  apiRequest("delete", `${coursesAddress.delete}/${postId}`);
 
 /**코스 등록*/
 export const postCourses = (data: postCourse): Promise<postCourse> => apiRequest("post", coursesAddress.get, data);
