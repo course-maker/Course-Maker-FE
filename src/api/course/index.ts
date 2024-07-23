b74e14815b5ef35a8aa455541c2eb1020c42e7ef
박지원 <jackgg12322@gmail.com>
2024-07-23 14:20:51 +0900

    Refactor: "course api 코드 수정"

--- src/api/course/index.ts
+++ src/api/course/index.ts
@@ -3,16 +3,14 @@ import { apiRequest } from "../axios";
 import { postCourse } from "./register";
 import { Courses, Course } from "./type";
 
-//코스 정보
-export const getCourse = (params: string): Promise<Courses> => apiRequest("get", `${coursesAddress.get}?${params}`);
+// 코스 목록 조회
+export const getCourse = (params: string): Promise<Courses> => apiRequest("get", `${coursesAddress.getList}?${params}`);
 
-/**코스 상세정보 조회하기*/
-export const getCourseDetail = (postId: number): Promise<Course> =>
-  apiRequest("get", `${coursesAddress.get}/${postId}`);
+// 코스 상세정보 조회
+export const getCourseDetail = (id: number): Promise<Course> => apiRequest("get", coursesAddress.getDetail(id));
 
-/**코스 삭제하기*/
-export const deleteCourseDetail = (postId: number): Promise<Course> =>
-  apiRequest("delete", `${coursesAddress.delete}/${postId}`);
+// 코스 삭제
+export const deleteCourseDetail = (id: number): Promise<Course> => apiRequest("delete", coursesAddress.delete(id));
 
-/**코스 등록*/
-export const postCourses = (data: postCourse): Promise<postCourse> => apiRequest("post", coursesAddress.get, data);
+// 코스 등록
+export const createCourse = (data: postCourse): Promise<postCourse> => apiRequest("post", coursesAddress.create, data);
