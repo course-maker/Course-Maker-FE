d2b673f4e8b51725bfc748a209dfa61d9c3d379a
박지원 <jackgg12322@gmail.com>
2024-07-30 14:27:22 +0900

    Refactor: "useToast Hook 정의 및 handleLinkCopy 함수 수정"

---
+++ src/hooks/useToast.ts
@@ -0,0 +1,34 @@
+import { toast, ToastContent, ToastOptions } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+type ToastType = "success" | "error" | "warning" | "default";
+
+const useToast = () => {
+  const showToast = (message: ToastContent, type: ToastType = "default") => {
+    const config: ToastOptions = {
+      position: "top-center",
+      autoClose: 3000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      rtl: false,
+      pauseOnFocusLoss: false,
+      draggable: false,
+      pauseOnHover: false,
+    };
+
+    switch (type) {
+      case "success":
+        return toast.success(message, config);
+      case "error":
+        return toast.error(message, config);
+      case "warning":
+        return toast.warning(message, config);
+      default:
+        return toast(message, config);
+    }
+  };
+
+  return showToast;
+};
+
+export default useToast;
