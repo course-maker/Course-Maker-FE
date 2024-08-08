5e55cb878d06f470faf56424c815e7bfcf768841
김하은 <han.kim0904@gmail.com>
2024-08-08 17:07:56 +0900

    Feat: 로그아웃 성공 시 모달 창 뜨게 하기

--- src/components/commons/NavigationBar/RightButtons.tsx
+++ src/components/commons/NavigationBar/RightButtons.tsx
@@ -1,35 +1,60 @@
-import styles from "./RightButtons.module.scss";
-import classNames from "classnames/bind";
 import Button from "@/components/commons/Button";
-import { useNavigate } from "react-router-dom";
+import { AlertModal } from "@/components/domains/signIn/AlertModal";
+import { MODALS } from "@/constants/modals";
 import { PAGE_PATH } from "@/constants/pagePath";
 import { useLogoutMutation } from "@/hooks/useLogoutMutation";
+import classNames from "classnames/bind";
+import { KeyboardEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./RightButtons.module.scss";
 
 const cx = classNames.bind(styles);
 
 const RightButtons = ({ isAuth }: { isAuth: boolean | null }) => {
   const navigate = useNavigate();
-  const { logout } = useLogoutMutation();
+  const { logout, isModalOpen, setIsModalOpen } = useLogoutMutation();
   const { signIn, signUp } = PAGE_PATH;
 
-  return isAuth ? (
-    <div className={cx("container")}>
-      {/* <Button color="emerald" variant="primary" size="small">
+  const handleKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === "Escape") {
+      setIsModalOpen(false);
+    }
+  };
+
+  return (
+    <>
+      {isAuth ? (
+        <div className={cx("container")}>
+          {/* <Button color="emerald" variant="primary" size="small">
         마이페이지
       </Button> */}
-      <Button color="emerald" variant="secondary" size="small" onClick={() => logout()}>
-        로그아웃
-      </Button>
-    </div>
-  ) : (
-    <div className={cx("container")}>
-      <Button color="emerald" variant="primary" size="small" onClick={() => navigate(signIn)}>
-        로그인
-      </Button>
-      <Button color="emerald" variant="secondary" size="small" onClick={() => navigate(signUp)}>
-        회원가입
-      </Button>
-    </div>
+          <Button color="emerald" variant="secondary" size="small" onClick={() => logout()}>
+            로그아웃
+          </Button>
+        </div>
+      ) : (
+        <div className={cx("container")}>
+          <Button color="emerald" variant="primary" size="small" onClick={() => navigate(signIn)}>
+            로그인
+          </Button>
+          <Button color="emerald" variant="secondary" size="small" onClick={() => navigate(signUp)}>
+            회원가입
+          </Button>
+        </div>
+      )}
+      <AlertModal
+        isOpen={isModalOpen}
+        content={MODALS.successLogout.message}
+        hasCloseBtn={MODALS.successLogout.hasCloseBtn}
+        onCloseClick={() => {
+          setIsModalOpen(false);
+        }}
+        onConfirmClick={() => {
+          setIsModalOpen(false);
+        }}
+        onKeyDown={handleKeyDown}
+      />
+    </>
   );
 };
 
