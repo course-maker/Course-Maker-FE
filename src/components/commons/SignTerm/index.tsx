33153a65c759e92e04a8c1ccaca599de1c3e025e
김하은 <han.kim0904@gmail.com>
2024-08-09 19:49:55 +0900

    Feat: 이용약관 모달 UI 구현(세부 내용 제외)

--- src/components/commons/SignTerm/index.tsx
+++ src/components/commons/SignTerm/index.tsx
@@ -1,6 +1,7 @@
+import { TermModal } from "@/components/domains/signUp/TermModal";
 import { IMAGES } from "@/constants/images";
 import classNames from "classnames/bind";
-import { ReactNode } from "react";
+import { KeyboardEvent, ReactNode, useState } from "react";
 import Image from "../Image";
 import styles from "./SignTerm.module.scss";
 
@@ -13,20 +14,35 @@ interface SignTermProps {
 }
 
 const SignTerm = ({ id, children, onChange }: SignTermProps) => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const closeModal = () => setIsModalOpen(false);
+  const handleKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === "Escape") {
+      closeModal();
+    }
+  };
   return (
-    <div className={cx("container", { all: id === "all" })}>
-      <div className={cx("box")}>
-        <input className={cx("checkbox")} id={id} type="checkbox" onChange={onChange} />
-        <label className={cx("label")} htmlFor={id}>
-          {children}
-        </label>
+    <>
+      <div className={cx("container", { all: id === "all" })}>
+        <div className={cx("box")}>
+          <input className={cx("checkbox")} id={id} type="checkbox" onChange={onChange} />
+          <label className={cx("label")} htmlFor={id}>
+            {children}
+          </label>
+        </div>
+        {id !== "all" && (
+          <button
+            className={cx("button")}
+            type="button"
+            onClick={() => {
+              setIsModalOpen(true);
+            }}>
+            <Image imageInfo={IMAGES.termMore} />
+          </button>
+        )}
       </div>
-      {id !== "all" && (
-        <button className={cx("button")} type="button">
-          <Image imageInfo={IMAGES.termMore} />
-        </button>
-      )}
-    </div>
+      <TermModal isOpen={isModalOpen} onCloseClick={closeModal} onKeyDown={handleKeyDown} />
+    </>
   );
 };
 export default SignTerm;
