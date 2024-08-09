adf658493fa8db9870b8ff079e89d9c899dbcb42
김하은 <han.kim0904@gmail.com>
2024-08-09 20:55:28 +0900

    Feat: 이용약과 체크박스 기능 구현

--- src/components/domains/signUp/SignTerms.tsx
+++ src/components/domains/signUp/SignTerms.tsx
@@ -1,15 +1,53 @@
 import SignTerm from "@/components/commons/SignTerm";
 import classNames from "classnames/bind";
+import { useEffect, useState } from "react";
 import styles from "./SignTerms.module.scss";
 
 const cx = classNames.bind(styles);
-const SignTerms = () => {
+
+interface SignTermsProps {
+  setAreTermsAccepted: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const SignTerms = ({ setAreTermsAccepted }: SignTermsProps) => {
+  const [terms, setTerms] = useState({
+    all: false,
+    first: false,
+    second: false,
+  });
+
+  const handleCheckboxChange = (id: string) => {
+    setTerms((prevTerms) => {
+      const newTerms = { ...prevTerms, [id]: !prevTerms[id as keyof typeof terms] };
+
+      if (id === "all") {
+        const allChecked = newTerms.all;
+        newTerms.first = allChecked;
+        newTerms.second = allChecked;
+      } else {
+        newTerms.all = newTerms.first && newTerms.second;
+      }
+
+      return newTerms;
+    });
+  };
+
+  useEffect(() => {
+    setAreTermsAccepted(terms.all && terms.first && terms.second);
+  }, [terms, setAreTermsAccepted]);
+
   return (
     <div>
       <div className={cx("container")}>
-        <SignTerm id="all">약관 전체 동의</SignTerm>
-        <SignTerm id="first">이용 약관에 동의합니다.</SignTerm>
-        <SignTerm id="second">개인정보수집・이용에 동의합니다.</SignTerm>
+        <SignTerm id="all" checked={terms.all} onChange={() => handleCheckboxChange("all")}>
+          약관 전체 동의
+        </SignTerm>
+        <SignTerm id="first" checked={terms.first} onChange={() => handleCheckboxChange("first")}>
+          이용 약관에 동의합니다.
+        </SignTerm>
+        <SignTerm id="second" checked={terms.second} onChange={() => handleCheckboxChange("second")}>
+          개인정보수집・이용에 동의합니다.
+        </SignTerm>
       </div>
       <p className={cx("message")}>※ 미동의 시 가입이 제한될 수 있습니다. </p>
     </div>
