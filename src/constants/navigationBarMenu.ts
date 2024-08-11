0dc17627be693acc77a62842c4fb6c2f6f098979
김하은 <han.kim0904@gmail.com>
2024-08-11 19:32:19 +0900

    Feat: 모바일 화면의 하단 TabBar 구현

--- src/constants/navigationBarMenu.ts
+++ src/constants/navigationBarMenu.ts
@@ -1,20 +1,29 @@
+import { IMAGES } from "./images";
 import { PAGE_PATH } from "./pagePath";
 
 export const NAVIGATION_BAR_MENU = {
   home: {
     title: "홈",
+    tabTitle: "홈",
     path: PAGE_PATH.home,
+    tabImage: IMAGES.BlackHomeIcon,
   },
   search: {
     title: "나만의 여행찾기",
+    tabTitle: "검색",
     path: PAGE_PATH.search,
+    tabImage: IMAGES.BlackSearchIcon,
   },
   courseRegister: {
     title: "코스 등록하기",
+    tabTitle: "코스",
     path: PAGE_PATH.courseRegister,
+    tabImage: IMAGES.BlackMapIcon,
   },
   destinationRegister: {
     title: "여행지 등록하기",
+    tabTitle: "여행지",
     path: PAGE_PATH.destinationRegister,
+    tabImage: IMAGES.BlackPointerIcon,
   },
 };
