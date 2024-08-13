0df05cd0231e3ccf4904f3961379fd65c5617b0d
김하은 <han.kim0904@gmail.com>
2024-08-13 20:09:37 +0900

    Design: TitleInput 스타일 변경

--- src/components/domains/destinationRegister/TitleInput.tsx
+++ src/components/domains/destinationRegister/TitleInput.tsx
@@ -1,14 +1,14 @@
 import { forwardRef, InputHTMLAttributes } from "react";
 
-import styles from "./TitleInput.module.scss";
 import classNames from "classnames/bind";
+import styles from "./TitleInput.module.scss";
 
 const cx = classNames.bind(styles);
 
 interface TitleInputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 const TitleInput = forwardRef<HTMLInputElement, TitleInputProps>(({ ...props }, ref) => {
-  return <input className={cx("input")} ref={ref} {...props} />;
+  return <input className={cx("input")} ref={ref} maxLength={30} {...props} />;
 });
 
 export default TitleInput;
