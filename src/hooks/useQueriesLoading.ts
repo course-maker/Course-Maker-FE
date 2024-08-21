f294053eb0f3aeeac86e3752498f7d7819af088b
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-08-21 16:59:49 +0900

    Feat: "쿼리 로딩여부 커스텀 훅 생성"

--- src/hooks/useQueriesLoading.ts
+++ src/hooks/useQueriesLoading.ts
@@ -1,1 +1,22 @@
-export function useQueriesLoading() {}
+import { useState, useEffect } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+
+export function useQueriesLoading() {
+  const client = useQueryClient();
+  const queries = client.getQueryCache().findAll();
+  const [queryChangedCount, setQueryChangedCount] = useState(0);
+  useEffect(() => {
+    if (queries) {
+      setQueryChangedCount(queries.length);
+    }
+  }, []);
+  useEffect(() => {
+    if (queries.every(({ state }) => state.status !== "loading") && queryChangedCount >= 0) {
+      setQueryChangedCount((prevState) => prevState - 1);
+    }
+  });
+  if (queryChangedCount < 0) {
+    return false;
+  }
+  return true;
+}
