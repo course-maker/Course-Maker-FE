a4be4998fb640297d5dec1248975c92e6a792a52
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-09-03 23:07:02 +0900

    Feat: "loading명 수정"

--- src/hooks/course/queries/useGetCourseQuery.ts
+++ src/hooks/course/queries/useGetCourseQuery.ts
@@ -2,11 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import { getCourse } from "@/api/course";
 
 export function useGetCourseQuery(param: string) {
-  const { data: courseData, isLoading } = useQuery({
+  const { data: courseData, isLoading: isCourseLoading } = useQuery({
     queryKey: ["course", param],
     queryFn: () => getCourse(param),
     enabled: !!param,
   });
 
-  return { courseData, isLoading };
+  return { courseData, isCourseLoading };
 }
