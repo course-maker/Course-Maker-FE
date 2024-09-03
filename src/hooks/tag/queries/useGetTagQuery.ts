264d688fc5e29dbf4e0357edca8515f0f5540e29
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-09-03 23:07:02 +0900

    Feat: "loading명 수정"

--- src/hooks/tag/queries/useGetTagQuery.ts
+++ src/hooks/tag/queries/useGetTagQuery.ts
@@ -2,10 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import { getTag } from "@/api/tag";
 
 export function useGetTagQuery() {
-  const { data: tagData, isLoading } = useQuery({
+  const { data: tagData, isLoading: isTagLoading } = useQuery({
     queryKey: ["tag"],
     queryFn: () => getTag(),
   });
 
-  return { tagData, isLoading };
+  return { tagData, isTagLoading };
 }
