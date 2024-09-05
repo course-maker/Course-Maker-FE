233a2bde677dea8b77f05624323cbeab4e3ee68f
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-09-05 16:00:12 +0900

    Feat: "태그 리스트 정보 커스텀 훅"

---
+++ src/hooks/business/useBadgeListViewModel.ts
@@ -0,0 +1,17 @@
+import { useRecoilState } from "recoil";
+import { useGetTagQuery } from "@/hooks/tag/queries/useGetTagQuery";
+import { CourseBadgesState, DestinationBadgesState } from "@/recoil/serviceAtom";
+
+export const useBadgeListViewModel = () => {
+  const { tagData, isTagLoading } = useGetTagQuery();
+  const [CourseBadges, setCourseBadges] = useRecoilState<tagResponseDto[]>(CourseBadgesState);
+  const [DestinationBadges, setDestinationBadges] = useRecoilState<tagResponseDto[]>(DestinationBadgesState);
+
+  //태그 데이터 패칭 여부 확인
+  if (isTagLoading) {
+    console.log("Loading tags data...");
+    return { isTagLoading, tagData: null };
+  }
+
+  return { isTagLoading, tagData, DestinationBadges, CourseBadges };
+};
