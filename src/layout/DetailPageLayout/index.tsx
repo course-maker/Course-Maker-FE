0e32aff63bf00a4a8ef10b3dc148e92330ea7b4c
김하은 <han.kim0904@gmail.com>
2024-09-07 14:11:58 +0900

    Feat: 상세 페이지 메인 부분 반응형 구현 및 토큰 재발급 url 수정

--- src/layout/DetailPageLayout/index.tsx
+++ src/layout/DetailPageLayout/index.tsx
@@ -1,4 +1,8 @@
+import classNames from "classnames/bind";
 import { ReactNode } from "react";
+import styles from "./DetailPageLayout.module.scss";
+
+const cx = classNames.bind(styles);
 
 interface DetailPageLayoutProps {
   header: ReactNode;
@@ -8,10 +12,10 @@ interface DetailPageLayoutProps {
 
 const DetailPageLayout = ({ header, main }: DetailPageLayoutProps) => {
   return (
-    <>
-      {header}
-      {main}
-    </>
+    <div className={cx("container")}>
+      <header className={cx("header")}>{header}</header>
+      <main className={cx("main")}>{main}</main>
+    </div>
   );
 };
 export default DetailPageLayout;
