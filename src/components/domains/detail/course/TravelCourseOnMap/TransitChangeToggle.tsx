b2132fc27f41fb45def145b91e52e75f8bd12f82
김하은 <han.kim0904@gmail.com>
2024-09-08 23:59:55 +0900

    Feat: 여행지 클릭 시 해당 여행지가 지도에서 확대되는 기능 구현

---
+++ src/components/domains/detail/course/TravelCourseOnMap/TransitChangeToggle.tsx
@@ -0,0 +1,8 @@
+const TransitChangeToggle = () => {
+  return (
+    <>
+      <h1>TransitChangeToggle Component</h1>
+    </>
+  );
+};
+export default TransitChangeToggle;
--- src/components/domains/detail/course/TravelCourseOnMap/TravelMap.tsx
+++ src/components/domains/detail/course/TravelCourseOnMap/TravelMap.tsx
@@ -1,11 +1,13 @@
 import { CourseDestination } from "@/api/course/register";
+import { Location, LocationWithId } from "@/type/type";
 import { useEffect } from "react";
 import { Map, MapMarker, Polyline, useMap } from "react-kakao-maps-sdk";
 interface TravelMapProps {
   destinations: CourseDestination[];
+  selectedLocation: LocationWithId | null;
 }
 
-const TravelMap = ({ destinations }: TravelMapProps) => {
+const TravelMap = ({ destinations, selectedLocation }: TravelMapProps) => {
   const positions = destinations.map((destination) => ({
     lat: destination.destination.location.latitude,
     lng: destination.destination.location.longitude,
@@ -13,25 +15,29 @@ const TravelMap = ({ destinations }: TravelMapProps) => {
 
   return (
     <Map center={positions[0]} style={{ width: "100%", height: "400px" }}>
-      <MarkersAndPolyline positions={positions} />
+      <MarkersAndPolyline positions={positions} selectedLocation={selectedLocation} />
     </Map>
   );
 };
 
 interface MarkersAndPolylineProps {
-  positions: { lat: number; lng: number }[];
+  positions: Location[];
+  selectedLocation: LocationWithId | null;
 }
 
-const MarkersAndPolyline = ({ positions }: MarkersAndPolylineProps) => {
+const MarkersAndPolyline = ({ positions, selectedLocation }: MarkersAndPolylineProps) => {
   const map = useMap();
 
   useEffect(() => {
-    if (map && positions.length > 0) {
+    if (map && selectedLocation) {
+      map.setCenter(new kakao.maps.LatLng(selectedLocation.lat, selectedLocation.lng));
+      map.setLevel(1);
+    } else if (map && positions.length > 0) {
       const bounds = new kakao.maps.LatLngBounds();
       positions.forEach((pos) => bounds.extend(new kakao.maps.LatLng(pos.lat, pos.lng)));
       map.setBounds(bounds);
     }
-  }, [map, positions]);
+  }, [map, selectedLocation, positions]);
 
   return (
     <>
