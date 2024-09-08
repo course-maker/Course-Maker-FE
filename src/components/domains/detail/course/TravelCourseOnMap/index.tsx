8f5687a309d1a681bfd1eb777054d57cf114fc93
김하은 <han.kim0904@gmail.com>
2024-09-08 22:35:30 +0900

    Feat: 코스 상세페이지에서 날짜별 버튼을 누르면 해당 여행지및 지도에 경로가 나오는 기능 구혀

--- src/components/domains/detail/course/TravelCourseOnMap/index.tsx
+++ src/components/domains/detail/course/TravelCourseOnMap/index.tsx
@@ -1,31 +1,39 @@
-// import { getCourseDetail } from "@/api/course";
-// import { defaultCourseDetail } from "@/constants/defaultValues";
-// import { useQuery } from "@tanstack/react-query";
-// import { useParams } from "react-router-dom";
-import DestinationCard from "./DestinationCard";
-import TransitTimeChip from "./TransitTimeChip";
+import { getCourseDetail } from "@/api/course";
+import { defaultCourseDetail } from "@/constants/defaultValues";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+
+import DestinationList from "./DestinationList";
+import TravelMap from "./TravelMap";
 
 const TravelCourseOnMap = () => {
-  // const { id } = useParams<{ id: string }>();
+  const [selectedDate, setSelectedDate] = useState(1);
 
-  // const { data: courseDetailData } = useQuery({
-  //   queryKey: ["courseDetailData", id],
-  //   queryFn: () => getCourseDetail(Number(id)),
-  //   retry: 0,
-  // });
+  const { id } = useParams<{ id: string }>();
 
-  // const courseDetail = courseDetailData ?? defaultCourseDetail;
+  const { data: courseDetailData } = useQuery({
+    queryKey: ["courseDetailData", id],
+    queryFn: () => getCourseDetail(Number(id)),
+    retry: 0,
+  });
+
+  const courseDetail = courseDetailData ?? defaultCourseDetail;
+
+  const selectedDestinations = courseDetail.courseDestinations.filter(
+    (destination) => destination.date === selectedDate,
+  );
 
   return (
     <>
-      <p>여행지를 클릭하면 여행지 위치를 확인할 수 있습니다.</p>
-      <DestinationCard number={1} title="해동용궁사" address="부산 기장군 기장읍 용궁길 86 해동용궁사" />
-      <TransitTimeChip transit="private" time="100분" />
-      <DestinationCard
-        number={12}
-        title="해동용궁사해동용궁사해동용궁사"
-        address="부산 기장군 기장읍 용궁길 86 해동용궁사부산 기장군 기장읍 용궁길 86 해동용궁사"
+      <DestinationList
+        duration={courseDetail.duration}
+        courseDestinations={courseDetail.courseDestinations}
+        selectedDestinations={selectedDestinations}
+        selectedDate={selectedDate}
+        setSelectedDate={setSelectedDate}
       />
+      <TravelMap destinations={selectedDestinations} />
     </>
   );
 };
