0ead6ad1156af8c98557422a513783c679837370
김하은 <han.kim0904@gmail.com>
2024-09-08 13:43:05 +0900

    Refactor: 상세페이지 관련 폴더 구조 정리

--- src/pages/CourseDetailPage/index.tsx
+++ src/pages/CourseDetailPage/index.tsx
@@ -1,9 +1,9 @@
-import CourseDetailInfo from "@/components/domains/DetailPage/CourseDetail/CourseDetailInfo/CourseDetailInfo";
-import CourseHeader from "@/components/domains/DetailPage/CourseDetail/Header/CourseHeader";
+import CourseHeader from "@/components/domains/detail/course/CourseHeader/CourseHeader";
+import CourseMain from "@/components/domains/detail/course/CourseMain/CourseMain";
 import DetailPageLayout from "@/layout/DetailPageLayout";
 
 const CourseDetailPage = () => {
-  return <DetailPageLayout type="course" header={<CourseHeader />} main={<CourseDetailInfo />} />;
+  return <DetailPageLayout type="course" header={<CourseHeader />} main={<CourseMain />} />;
 };
 
 export default CourseDetailPage;
