2a800f8c5b1e598a4cc432f71712c601893c8a0d
김하은 <han.kim0904@gmail.com>
2024-09-15 01:17:48 +0900

    Feat: 상세페이지 리뷰 카드 구현

--- src/components/domains/detail/Review/index.tsx
+++ src/components/domains/detail/Review/index.tsx
@@ -3,6 +3,7 @@ import classNames from "classnames/bind";
 import { useState } from "react";
 import FilterButtons from "./FilterButtons";
 import styles from "./Review.module.scss";
+import ReviewCardList from "./ReviewCardList";
 import ReviewForm from "./ReviewForm";
 import StarScore from "./StarScore";
 
@@ -21,10 +22,13 @@ const Review = () => {
         <StarScore score={4.2} />
         <FilterButtons selectedFilter={selectedFilter} onClick={handleFilterClick} />
       </header>
-      <section className={cx("form")}>
+      <div className={cx("form")}>
         <h2 className={cx("form-title")}>리뷰 작성하기</h2>
         <ReviewForm />
-      </section>
+      </div>
+      <article className={cx("review")}>
+        <ReviewCardList />
+      </article>
     </div>
   );
 };
