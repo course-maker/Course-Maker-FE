5f80a9094284bdfa96df9707d345fbed3e130fb0
김하은 <han.kim0904@gmail.com>
2024-09-23 16:36:21 +0900

    Fix: quill 스타일 적용 안되는 부분 적용

--- src/components/domains/detail/destination/DestinationInfo/DestinationInfo.tsx
+++ src/components/domains/detail/destination/DestinationInfo/DestinationInfo.tsx
@@ -1,7 +1,29 @@
+import { getDestinationApi } from "@/api/destination";
+import { defaultDestinationDetail } from "@/constants/defaultValues";
+import { useQuery } from "@tanstack/react-query";
+import classNames from "classnames/bind";
+import { useParams } from "react-router-dom";
+import Description from "../../Description/Description";
+import styles from "./DestinationInfo.module.scss";
+
+const cx = classNames.bind(styles);
+
 const DestinationInfo = () => {
+  const { id } = useParams<{ id: string }>();
+
+  const { data: destinationDetailData } = useQuery({
+    queryKey: ["destinationDetailData", id],
+    queryFn: () => getDestinationApi(Number(id)),
+    retry: 0,
+  });
+
+  const destinationDetail = destinationDetailData ?? defaultDestinationDetail;
+
   return (
     <>
-      <h1>DestinationInfo Component</h1>
+      <div className={cx("description")}>
+        <Description content={destinationDetail.content} />
+      </div>
     </>
   );
 };
