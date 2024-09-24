30a58476cbd3bb72c6886fc624755aa18db2403e
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-09-24 21:39:55 +0900

    Feat: "경로 수정 데이터 연동"

---
+++ src/components/domains/courseRegister/DestinationList.tsx
@@ -0,0 +1,61 @@
+import { CourseDestination } from "@/api/course/register";
+import { LocationWithId } from "@/type/type";
+import classNames from "classnames/bind";
+import DateTab from "@/components/domains/detail/course/CourseInfo/TravelCourseOnMap/DateTab";
+import DestinationCard from "@/components/domains/detail/course/CourseInfo/TravelCourseOnMap/DestinationCard";
+import styles from "@/components/domains/detail/course/CourseInfo/TravelCourseOnMap/DestinationList.module.scss";
+import TransitTimeChip from "@/components/domains/detail/course/CourseInfo/TravelCourseOnMap/TransitTimeChip";
+
+const cx = classNames.bind(styles);
+
+interface DestinationListProps {
+  duration: number;
+  selectedDestinations: CourseDestination[];
+  selectedDate: number;
+  onCardClick: (day: number) => void;
+  onChipClick: (index: number) => void;
+  selectedLocation: LocationWithId | null;
+  setSelectedLocation: React.Dispatch<React.SetStateAction<LocationWithId | null>>;
+}
+
+const DestinationList = ({
+  duration,
+  selectedDestinations,
+  selectedDate,
+  onCardClick,
+  onChipClick,
+  selectedLocation,
+  setSelectedLocation,
+}: DestinationListProps) => {
+  const days = Array.from({ length: duration }, (_, i) => i + 1);
+
+  return (
+    <div className={cx("container")}>
+      <div>
+        <DateTab days={days} selectedDate={selectedDate} onClick={onCardClick} />
+      </div>
+      <p className={cx("text")}>여행지를 클릭하면 여행지 위치를 확인할 수 있습니다.</p>
+      <div className={cx("cards")}>
+        {selectedDestinations?.map((destination, index) => (
+          <div className={cx("card")} key={destination.visitOrder}>
+            <DestinationCard
+              number={destination.visitOrder}
+              title={destination.destination.name}
+              address={destination.destination.location.address}
+              isSelected={selectedLocation?.id === destination.destination.id}
+              onClick={() => {
+                setSelectedLocation({
+                  id: destination.destination.id,
+                  lat: destination.destination.location.latitude,
+                  lng: destination.destination.location.longitude,
+                });
+              }}
+            />
+            {selectedDestinations.length - 1 !== index && <TransitTimeChip onClick={() => onChipClick(index)} />}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default DestinationList;
--- src/components/domains/courseRegister/DestinationsInputController.tsx
+++ src/components/domains/courseRegister/DestinationsInputController.tsx
@@ -1,5 +1,6 @@
 import { Control, Controller, FieldValues, Path } from "react-hook-form";
-import TitleInput from "./TitleInput";
+
+import TravelCourseOnMap from "./TravelCourseOnMap";
 
 interface DestinationsInputControllerProps<ControlType extends FieldValues> {
   formFieldName: Path<ControlType>;
@@ -16,7 +17,7 @@ const DestinationsInputController = <ControlType extends FieldValues>({
     <Controller
       name={formFieldName}
       control={control}
-      render={({ field }) => <TitleInput placeholder="여행지 이름을 입력하세요." disabled={disabled} {...field} />}
+      render={({ field }) => <TravelCourseOnMap courseDetail={field.value} disabled={disabled} {...field} />}
     />
   );
 };
