0276d8c095afd880dff4bb8b8833ebe3fd2b0f27
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-09-27 20:15:38 +0900

    Feat: "경로 설정 예외처리"

--- src/components/domains/courseRegister/DestinationList.tsx
+++ src/components/domains/courseRegister/DestinationList.tsx
@@ -5,6 +5,8 @@ import DateTab from "@/components/domains/detail/course/CourseInfo/TravelCourseO
 import DestinationCard from "@/components/domains/detail/course/CourseInfo/TravelCourseOnMap/DestinationCard";
 import styles from "@/components/domains/detail/course/CourseInfo/TravelCourseOnMap/DestinationList.module.scss";
 import TransitTimeChip from "@/components/domains/detail/course/CourseInfo/TravelCourseOnMap/TransitTimeChip";
+import Image from "@/components/commons/Image";
+import { IMAGES } from "@/constants/images";
 
 const cx = classNames.bind(styles);
 
@@ -16,6 +18,7 @@ interface DestinationListProps {
   onChipClick: (index: number) => void;
   selectedLocation: LocationWithId | null;
   setSelectedLocation: React.Dispatch<React.SetStateAction<LocationWithId | null>>;
+  handleOpenModal: () => void;
 }
 
 const DestinationList = ({
@@ -26,9 +29,9 @@ const DestinationList = ({
   onChipClick,
   selectedLocation,
   setSelectedLocation,
+  handleOpenModal,
 }: DestinationListProps) => {
   const days = Array.from({ length: duration }, (_, i) => i + 1);
-
   console.log(duration);
   return (
     <div className={cx("container")}>
@@ -36,26 +39,33 @@ const DestinationList = ({
         <DateTab days={days} selectedDate={selectedDate} onClick={onCardClick} />
       </div>
       <p className={cx("text")}>여행지를 클릭하면 여행지 위치를 확인할 수 있습니다.</p>
-      <div className={cx("cards")}>
-        {selectedDestinations?.map((destination, index) => (
-          <div className={cx("card")} key={destination.visitOrder}>
-            <DestinationCard
-              number={destination.visitOrder}
-              title={destination.destination.name}
-              address={destination.destination.location.address}
-              isSelected={selectedLocation?.id === destination.destination.id}
-              onClick={() => {
-                setSelectedLocation({
-                  id: destination.destination.id,
-                  lat: destination.destination.location.latitude,
-                  lng: destination.destination.location.longitude,
-                });
-              }}
-            />
-            {selectedDestinations.length - 1 !== index && <TransitTimeChip onClick={() => onChipClick(index)} />}
-          </div>
-        ))}
-      </div>
+      {selectedDestinations.length > 0 ? (
+        <div className={cx("cards")}>
+          {selectedDestinations?.map((destination, index) => (
+            <div className={cx("card")} key={destination.visitOrder}>
+              <DestinationCard
+                number={destination.visitOrder}
+                title={destination.destination.name}
+                address={destination.destination.location.address}
+                isSelected={selectedLocation?.id === destination.destination.id}
+                onClick={() => {
+                  setSelectedLocation({
+                    id: destination.destination.id,
+                    lat: destination.destination.location.latitude,
+                    lng: destination.destination.location.longitude,
+                  });
+                }}
+              />
+              {selectedDestinations.length - 1 !== index && <TransitTimeChip onClick={() => onChipClick(index)} />}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className={cx("empty")} onClick={handleOpenModal}>
+          <Image className={cx("icon")} imageInfo={IMAGES.calendarGreyIcon} />
+          <div>경로 추가하기</div>
+        </div>
+      )}
     </div>
   );
 };
