e341a96b3791a624433957997ce3c03a638b0e21
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-09-27 16:23:05 +0900

    Feat: "여행기간, 인원 옵션 버튼 type 수정"

--- src/components/domains/courseRegister/SliderBar.tsx
+++ src/components/domains/courseRegister/SliderBar.tsx
@@ -11,7 +11,7 @@ const Slider: React.FC<SliderProps> = ({ type, value, onChange }) => {
   const options = type === "travelerCount" ? ["1인", "2인", "3인", "4인", "5인이상"] : ["1일", "2일", "3일"];
 
   const handleClick = (index: number) => {
-    onChange(index + 1); // index에 1을 더해서 1부터 시작하도록 설정
+    onChange(index + 1);
   };
 
   return (
@@ -19,6 +19,7 @@ const Slider: React.FC<SliderProps> = ({ type, value, onChange }) => {
       {options.map((option, index) => (
         <button
           key={index}
+          type="button"
           className={`${styles.sliderButton} ${value === index + 1 ? styles.active : ""}`}
           onClick={() => handleClick(index)}>
           {option}
