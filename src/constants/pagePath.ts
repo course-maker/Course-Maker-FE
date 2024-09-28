7c5860efa1c9e3b73d323c6c75c85d62d0dc3ff5
김하은 <han.kim0904@gmail.com>
2024-09-28 16:00:28 +0900

    Feat: 마이페이지 페이지 생성

--- src/constants/pagePath.ts
+++ src/constants/pagePath.ts
@@ -9,6 +9,10 @@ export const PAGE_PATH = {
   destinationRegister: "/destination/register", // 여행지 등록하기
   courseEdit: "/course/:id/edit", // 코스 수정하기
   destinationEdit: "/destination/:id/edit", // 여행지 수정하기
+  myPage: "/mypage", // 마이페이지
+  myPageLikes: "/mypage/likes", // 마이페이지 찜
+  myPageRank: "/mypage/rank", // 마이페이지 나의 등급
+  myPageTrips: "/mypage/trips", // 마이페이지 내가 만든 여행
   authKakao: "/auth/kakao/callback",
 };
 
