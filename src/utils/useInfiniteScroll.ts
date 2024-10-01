2b95553741e6b959fb34c10b7654223496d727b0
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-10-02 01:57:23 +0900

    Feat: "인피니트 스크롤 호출 코드 모듈화"

---
+++ src/utils/useInfiniteScroll.ts
@@ -0,0 +1,41 @@
+import { useCallback, useEffect, useRef } from "react";
+import { InfiniteData } from "@tanstack/react-query";
+
+interface UseInfiniteScrollOptions<T> {
+  data: InfiniteData<T, unknown> | undefined;
+  hasNextPage: boolean | undefined;
+  isFetchingNextPage: boolean;
+  fetchNextPage: () => void;
+}
+
+export function useInfiniteScroll<T>({
+  data,
+  hasNextPage,
+  isFetchingNextPage,
+  fetchNextPage,
+}: UseInfiniteScrollOptions<T>) {
+  const observerElem = useRef<HTMLDivElement | null>(null);
+
+  const handleObserver = useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      const [target] = entries;
+      if (target.isIntersecting && hasNextPage && !isFetchingNextPage) {
+        fetchNextPage();
+      }
+    },
+    [fetchNextPage, hasNextPage, isFetchingNextPage],
+  );
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleObserver, { threshold: 1.0 });
+    if (observerElem.current) observer.observe(observerElem.current);
+    return () => observer.disconnect();
+  }, [handleObserver]);
+
+  //   const allData = data?.pages.flatMap((page) => page.contents) ?? [];
+
+  return {
+    data,
+    observerElem,
+  };
+}
