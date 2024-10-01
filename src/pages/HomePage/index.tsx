cfdae5f2d227c7e70e93ced30d7f2006da02f77a
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-10-01 16:29:53 +0900

    Feat: "주석 삭제"

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -63,8 +63,6 @@ const HomePage = () => {
 
     // 리사이즈 이벤트 리스너 추가
     window.addEventListener("resize", handleResize);
-
-    // 컴포넌트 언마운트 시 이벤트 리스너 제거
     return () => {
       window.removeEventListener("resize", handleResize);
     };
