701f726846b3344973373eb96bb150e27282a278
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-10-15 18:10:16 +0900

    Fix: "ReactQueryDevtools 주석 처리"

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RecoilRoot } from "recoil";
 import PageRouter from "./PageRouter";
 
@@ -16,7 +16,7 @@ function App() {
     <>
       <RecoilRoot>
         <QueryClientProvider client={queryClient}>
-          <ReactQueryDevtools initialIsOpen={false} />
+          {/* <ReactQueryDevtools initialIsOpen={false} /> */}
           <PageRouter />
         </QueryClientProvider>
       </RecoilRoot>
