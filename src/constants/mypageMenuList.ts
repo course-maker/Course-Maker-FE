a930c5a2fdc8264f3c24a13627109f3f8e00ec86
김하은 <han.kim0904@gmail.com>
2024-12-19 16:42:04 +0900

    Feat: 마이페이지 메뉴 이미지 추가 및 관련 constants 파일에 메뉴 정보 추가

--- src/constants/mypageMenuList.ts
+++ src/constants/mypageMenuList.ts
@@ -2,7 +2,10 @@ import { IMAGES } from "./images";
 import { PAGE_PATH } from "./pagePath";
 
 export const MYPAGE_MENU_LIST = [
-  { id: 1, icon: IMAGES.mypageMenuHeart, name: "찜", navigate: PAGE_PATH.myPageLikes },
-  { id: 2, icon: IMAGES.mypageMenuLevel, name: "나의 등급", navigate: PAGE_PATH.myPageRank },
+  { id: 1, icon: IMAGES.mypageMenuUserInfo, name: "회원 정보 변경", navigate: PAGE_PATH.myPageUserInfo },
+  { id: 2, icon: IMAGES.mypageMenuLevel, name: "내 등급/뱃지", navigate: PAGE_PATH.myPageRank },
   { id: 3, icon: IMAGES.mypageMenuTravel, name: "내가 만든 여행", navigate: PAGE_PATH.myPageTrips },
+  { id: 4, icon: IMAGES.mypageMenuHeart, name: "찜", navigate: PAGE_PATH.myPageLikes },
+  { id: 5, icon: IMAGES.mypageMenuReview, name: "내가 쓴 리뷰", navigate: PAGE_PATH.myPageReview },
+  { id: 6, icon: IMAGES.mypageMenuDelete, name: "회원 탈퇴", navigate: PAGE_PATH.myPageDelete },
 ];
